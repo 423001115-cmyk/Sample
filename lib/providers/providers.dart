b7132888990e065005d27eca52ea2f766b688011
Rovic Miguel I. Siazon <423000992@ntc.edu.ph>
2026-03-06 01:44:35 +0800

    Add Flutter TravelMate app

---
+++ lib/providers/providers.dart
@@ -0,0 +1,2 @@
+export 'trip_provider.dart';
+export 'theme_provider.dart';
---
+++ lib/screens/screens.dart
@@ -0,0 +1,5 @@
+export 'home_screen.dart';
+export 'trip_details_screen.dart';
+export 'add_trip_screen.dart';
+export 'add_destination_screen.dart';
+export 'add_activity_screen.dart';
